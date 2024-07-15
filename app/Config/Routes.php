02aea4250a0714a290979430806dcafbcfede523
difaalfian42 <146816934+difaalfian42@users.noreply.github.com>
2024-07-15 13:30:04 +0700

    Tugas 5

---
+++ app/Config/Routes.php
@@ -0,0 +1,30 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->group('produk', ['filter' => 'auth'], function($route) {
+    $route->get('/', 'ProdukController::index');
+    $route->post('/', 'ProdukController::create');
+    $route->post('edit/(:any)', 'ProdukController::edit/$1');
+    $route->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
+});
+
+$routes->get('profile', 'ProfileController::index', ['filter' => 'auth']);
